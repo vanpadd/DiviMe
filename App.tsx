60c6e91edab9d2629bfd936b5a68de52dd1c9e2d
Waldo van Paddenburg <vanpadd@Waldos-MacBook-Pro.local>
2021-02-12 14:40:31 +1300

    Add main input box

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
+  const [value, onChangeText] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerStyle}>
+        <FontAwesome
+          style={styles.searchStyle}
+          name="dollar"
+          size={24}
+          color="grey"
+        />
+        <TextInput
+          style={styles.inputStyle}
+          autoCorrect
+          autoCapitalize="none"
+          placeholder="Gross income"
+          value={value}
+          onChangeText={onChangeText}
+          blurOnSubmit
+        />
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +34,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  containerStyle: {
+    margin: 15,
+    flexDirection: 'row',
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+  },
+  searchStyle: {
+    marginHorizontal: 15,
+    alignSelf: 'center',
+  },
+  inputStyle: {
+    fontSize: 18,
+    height: 50,
+    backgroundColor: '#F0EEEE',
+    flex: 1,
   },
 });
